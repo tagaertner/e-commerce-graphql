a4ef7ab06ed3eb1171de99b16efd1959da82f79e
Tami <tamigaertner@outlook.com>
2026-04-07 11:45:38 -0400

    removed env var to fix render and gradio

--- gradio_ui/app.py
+++ gradio_ui/app.py
@@ -11,7 +11,7 @@ Loads components and assembles the UI.
 
     
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 10000))
+    # port = int(os.environ.get("PORT", 10000))
     demo = build_interface()
     
     
