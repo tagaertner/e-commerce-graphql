509d1523dc91d58283403d8f5b1e21b9f667d8e8
Tami <tamigaertner@outlook.com>
2026-04-07 11:50:25 -0400

    downgrade graio and update app

--- gradio_ui/app.py
+++ gradio_ui/app.py
@@ -11,9 +11,14 @@ Loads components and assembles the UI.
 
     
 if __name__ == "__main__":
-    # port = int(os.environ.get("PORT", 10000))
+  
+    port = int(os.environ.get("PORT", 10000))
     demo = build_interface()
     
-    
-    demo.launch(share=False)
+    demo.launch(
+        server_name="0.0.0.0", 
+        server_port=port,
+        share=False
+    )
+
  
