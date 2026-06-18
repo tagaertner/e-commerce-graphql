2655221ecce5aaf76d571941949ac4e415c55f59
Tami <tamigaertner@outlook.com>
2026-06-18 19:23:24 -0400

    fix gradio port

--- gradio_ui/app.py
+++ gradio_ui/app.py
@@ -1,5 +1,5 @@
 from interface import build_interface
-
+import os
 """
 app.py
 ------
@@ -14,6 +14,8 @@ Loads components and assembles the UI.
 
 if __name__ == "__main__":
     demo = build_interface()
-    demo.launch(server_name="0.0.0.0", server_port=4004)
-    input("Gradio app is running. Press Enter to stop...")
-    # TODO need to change ports
+    demo.launch(
+    server_name="0.0.0.0",
+    server_port=int(os.environ.get("PORT", 10000))
+
+)
